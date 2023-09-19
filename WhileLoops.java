15aca9f122f8a37adc6b121a52cac878f7871682
EvaBartolome <eva.bartolome@mrjaffesclass.com>
2023-09-18 21:29:00 -0700

    Completed Code

---
+++ WhileLoops.java
@@ -0,0 +1,24 @@
+
+public class WhileLoops
+{
+    public void run () 
+{
+    int i = 0;
+    while (i<15)
+    {
+        i++;
+    }
+    int k =10;
+    while (k > 5)
+    {
+        k--;
+    }
+    int a = 0;
+    int b= 15;
+    while (a < 10 && b <= 20) 
+    {
+        a++;
+        b++;
+    }
+}
+}
