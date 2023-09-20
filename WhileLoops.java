4338a3b64fa1ea214150729913b011702c03136b
Eva Bartolome <eva.bartolome@mrjaffesclass.com>
2023-09-20 11:40:14 -0700

    CHANGED METHOD

--- WhileLoops.java
+++ WhileLoops.java
@@ -5,21 +5,21 @@ public static void main(String[] args) {
     int i = 0;
     while (i < 15)
     {
-        i = i + 1;
+        i++;
         System.out.println("i ="+ i);
     }
     int k = 6;
     while (k > 5)
     {
-        k = k - 1;
+        k--;
         System.out.println("k =" + k);
     }
     int a = 0;
     int b = 20;
     while (a < 10 && b <= 20) 
     {
-        a = a + 1;
-        b = b - 1;
+        a++;
+        b++;
         System.out.println("a =" + a);
         System.out.println("b =" + b);
     }
