02a1f8ca7a49d68a6f38a2c947ec3ce316518cd9
EvaBartolome <eva.bartolome@mrjaffesclass.com>
2023-09-20 20:49:23 -0700

    commit message

--- WhileLoops.java
+++ WhileLoops.java
@@ -5,17 +5,17 @@ public static void main(String[] args) {
     int i = 0;
     while (i < 15)
     {
-        i++;
+        i++; //i= i + 1
         System.out.println("i ="+ i);
     }
     int k = 6;
     while (k > 5)
     {
-        k--;
+        k--; //k= k - 1
         System.out.println("k =" + k);
     }
     int a = 0;
-    int b = 20;
+    int b = 15;
     while (a < 10 && b <= 20) 
     {
         a++;
